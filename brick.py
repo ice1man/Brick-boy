d9e86c312b1c87dae547ff478dce2a29286e7fef
ice cube labs <105887752+ice1man@users.noreply.github.com>
2024-10-21 18:22:31 -0400

    Create brick.py

---
+++ brick.py
@@ -0,0 +1,40 @@
+import machine
+from machine import I2C
+from lcd_api import LcdApi
+from pico_i2c_lcd import I2cLcd
+import random
+from machine import PWM, Pin
+import time
+
+
+
+I2C_ADDR = 0x27
+I2C_NUM_ROWS = 2
+I2C_NUM_COLS = 16
+i2c = I2C(1, sda=machine.Pin(18), scl=machine.Pin(19), freq=400000)
+display_text = I2cLcd(i2c, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)    
+
+BUTTON_A_PIN = 15
+BUTTON_B_PIN = 14
+BUTTON_C_PIN = 13
+BUTTON_D_PIN = 12
+
+button_a = machine.Pin(BUTTON_A_PIN, machine.Pin.IN, machine.Pin.PULL_DOWN)
+button_b = machine.Pin(BUTTON_B_PIN, machine.Pin.IN, machine.Pin.PULL_DOWN)
+button_c = machine.Pin(BUTTON_C_PIN, machine.Pin.IN, machine.Pin.PULL_DOWN)
+button_d = machine.Pin(BUTTON_D_PIN, machine.Pin.IN, machine.Pin.PULL_DOWN)
+
+def key_input():
+    while True:
+        if button_a.value() == 1:
+            return("A")
+            break
+        if button_b.value() == 1:
+            return("B")
+            break
+        if button_c.value() == 1:
+            return("C")
+            break
+        if button_d.value() == 1:
+            return("D")
+            break
