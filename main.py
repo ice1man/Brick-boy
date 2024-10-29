815a1dc5a3229467d7002a100d2e1140b5810ce8
64bit-sys <isaede1735@student.iss.k12.nc.us>
2024-10-29 13:15:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import brick
 import rps
+import remote
 
 brick.display_text.putstr("welcome to the")
 brick.display_text.move_to(0,1)
 brick.display_text.putstr("brick boi")
 
 brick.display_text.clear()
-item = brick.pick(["rps"])
+item = brick.pick(["rps","ir"])
 if item == 0:
   rps.main()
+elif item == 1:
+  remote.main()
