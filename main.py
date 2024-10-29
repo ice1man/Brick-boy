7538702a07239a6aaa6b102e1b550093b042667e
64bit-sys <isaede1735@student.iss.k12.nc.us>
2024-10-29 11:38:34 -0400

    ad rps call

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import brick
+import rps
 
 brick.display_text.putstr("welcome to the")
 brick.display_text.move_to(0,1)
 brick.display_text.putstr("brick boi")
+
+brick.display_text.clear()
+item = brick.pick(["rps"])
+if item == 0:
+  rps.main()
