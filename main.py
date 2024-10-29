fc2c3e8c2e3c47f5392e28cf8d0386cbc57ad08e
64bit-sys <isaede1735@student.iss.k12.nc.us>
2024-10-29 16:24:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import brick
 import rps
 import remote
+import counter
 
 brick.display_text.putstr("welcome to the")
 brick.display_text.move_to(0,1)
@@ -12,3 +13,5 @@ if item == 0:
   rps.main()
 elif item == 1:
   remote.main()
+elif item == 2:
+  counter.main()
