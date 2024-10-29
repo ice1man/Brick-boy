e0de507af7f285e277820f595e9d9cab2da9ded7
64bit-sys <isaede1735@student.iss.k12.nc.us>
2024-10-29 13:27:15 -0400

    Update remote.py

--- remote.py
+++ remote.py
@@ -1,1 +1,12 @@
+import brick
 
+def main():
+  doing = brick.pick(["transmit","receive"])
+  if doing == 0:
+    while True:
+      device = brick.pick(["apple tv","infocus","nec","samsung"])
+      if device == 0:
+        action = brick.pick(["reboot","menu","play_pause","up","down","left","right","center"])
+        if action == 0:
+          
+  elif doing == 1:
