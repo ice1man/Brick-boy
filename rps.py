c69003afd9c170ecaaabc7a803b7ef8341bf4ef8
64bit-sys <isaede1735@student.iss.k12.nc.us>
2024-10-29 11:32:59 -0400

    Create rps.py

---
+++ rps.py
@@ -0,0 +1,38 @@
+import brick
+import random
+from time import sleep
+
+def main():
+  choice = brick.pick(["rock","paper","scissors"])
+  com = random.randint(0,2)
+  if choice == 0:
+    if com == 0:
+      brick.display_text.putstr("tie")
+    if com == 1:
+      brick.display_text.putstr("win")
+    if com == 2:
+      brick.display_text.putstr("you lose")
+  if choice == 1:
+    if com == 1:
+      brick.display_text.putstr("tie")
+    if com == 2:
+      brick.display_text.putstr("win")
+    if com == 0:
+      brick.display_text.putstr("you lose")
+  if choice == 2:
+    if com == 2:
+      brick.display_text.putstr("tie")
+    if com == 0:
+      brick.display_text.putstr("win")
+    if com == 1:
+      brick.display_text.putstr("you lose")
+  brick.key_input()
+  sleep(2)
+  brick.display_text.clear()
+  brick.display_text.putstr("do you want to play again")
+  sleep(2)  
+  if brick.pick(["yes","no"]) == 0:
+    brick.display_text.clear()
+    main()
+  else:
+    break
